e4a5d76f120f01d48f49a06057e699157bd46ac8
douglasmariz <douglasmariz.developer@gmail.com>
2022-04-30 19:36:04 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const App = () => {
+    const [calendar, setCalendar]: any = React.useState({});
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                dateClick={(values) => {
+                    handleOpen();
+                    setCalendar(values);
+                }}
+            />
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Full Calendar example
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{mt: 2}}>
+                        {`you clicked on  ${calendar.dateStr || ''}`}
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
+    )
+}
+
+export default App;
