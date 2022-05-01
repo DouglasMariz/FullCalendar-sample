6199085ae331022e0eaf37538c38f3b4d3ad9771
douglasmariz <douglasmariz.developer@gmail.com>
2022-05-01 14:12:28 -0300

    Exemplo de calendário com dados personalizados e exibição das informação em modal.

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,9 @@ import Modal from '@mui/material/Modal';
 
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timegridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import moment from "moment";
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -28,11 +30,54 @@ const App = () => {
     return (
         <div>
             <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin]}
+                plugins={[dayGridPlugin, interactionPlugin, timegridPlugin]}
                 initialView="dayGridMonth"
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                initialEvents={[
+                    {
+                        id: '1',
+                        title: 'Teste Ativo 1',
+                        start: moment('2022-05-02 12:00:00').toDate(),
+                        end: moment('2022-05-03 12:00:00').toDate(),
+                        status: 'Ativo',
+                        color: 'blue'
+                    },
+                    {
+                        id: '2',
+                        title: 'Teste Inativo',
+                        start: moment('2022-05-04 12:00:00').format('YYYY-MM-DD hh:mm:ss'),
+                        end: moment('2022-05-05 12:00:00').format('YYYY-MM-DD hh:mm:ss'),
+                        status: 'Inativo',
+                        color: 'red',
+                        textColor: 'black'
+                    },
+                    {
+                        id: '3',
+                        title: 'Teste Ativo 2',
+                        start: moment('2022-05-06').format('YYYY-MM-DD'),
+                        status: 'Ativo',
+                        color: 'blue'
+                    },
+                    {
+                        id: '4',
+                        title: 'Teste Inativo 2',
+                        start: moment('2022-05-07').toDate(),
+                        status: 'Inativo',
+                        color: 'red',
+                        textColor: 'black'
+                    },
+                ]}
                 dateClick={(values) => {
-                    handleOpen();
                     setCalendar(values);
+                    handleOpen();
+                }}
+                eventClick={(arg) => {
+                    const {id, title, startStr, endStr, backgroundColor, extendedProps: {status: status}} = arg.event;
+                    setCalendar({id, title, startStr, endStr, backgroundColor, status});
+                    handleOpen();
                 }}
             />
 
@@ -44,10 +89,24 @@ const App = () => {
             >
                 <Box sx={style}>
                     <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Full Calendar example
+                        {calendar.title || "Full Calendar example"}
                     </Typography>
                     <Typography id="modal-modal-description" sx={{mt: 2}}>
-                        {`you clicked on  ${calendar.dateStr || ''}`}
+                        {calendar.dateStr && `you clicked on day ${calendar.dateStr}`}
+                        <div>
+                            <div>
+                                {calendar.startStr && `Event start: ${calendar.startStr}`}
+                            </div>
+
+                            {calendar.endStr && `Event end: ${calendar.endStr}`}
+                            {calendar.status &&
+                                <div style={{
+                                    color: calendar.backgroundColor
+                                }}>
+                                    Status is: {calendar.status}
+                                </div>
+                            }
+                        </div>
                     </Typography>
                 </Box>
             </Modal>
